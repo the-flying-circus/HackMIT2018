b98ac461baee366622e12c31e01b3bef44c48f69
Eric Wang <ezwang1@gmail.com>
2018-09-15 15:46:07 -0400

    push more secret keys

--- secrets.py
+++ secrets.py
@@ -1,2 +1,9 @@
+# IBM Watson API - https://www.ibm.com/watson/developer/
 IBM_USER = "cdf15aa8-7275-4937-bbd7-ad92fc764628"
 IBM_PW = "cZ7PWsZFn6lq"
+
+# RevSpeech API - http://docs.rev.ai/en/latest/
+REVSPEECH_KEY = "01mWYR8yqQFhJ_J4sP0ECdBRCvEg1JAaK0gEbK7weZy_EYj3Jo4tPfB1HCEaQAhJwT0jOJsrzL60FQfliulyyzS3JR4KE"
+
+# Giphy API - https://developers.giphy.com/
+GIPHY_KEY = "jF1iIHcLeb0B22v8HVRPCwpw2HLkj1L9"
