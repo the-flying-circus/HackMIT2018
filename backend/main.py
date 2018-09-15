9875561f39567611bb9da62748c17bbed002d527
Grey Golla <bobobalink@gmail.com>
2018-09-15 14:24:14 -0400

    started flask framework stuff

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,10 @@
-import flask
+import eventlet
+from flask import Flask
+from flask_socketio import SocketIO
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sekrit'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
