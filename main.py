d7aba09fd94f0d09a9aa2e85652643c269255197
Eric Wang <ezwang1@gmail.com>
2018-09-15 14:41:04 -0400

    push index page

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 import eventlet
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sekrit'
 socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     socketio.run(app)
