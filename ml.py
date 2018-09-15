ab20c78d23cc6d4e6d3db737e9fd7434dc445f9b
Haicheng Charles Zhao <czhao39@gmail.com>
2018-09-15 15:53:09 -0400

    Create compatibility score function

--- ml.py
+++ ml.py
@@ -2,6 +2,8 @@
 
 import requests
 from pprint import pprint
+from typing import Tuple
+import math
 
 import secrets
 
@@ -26,8 +28,32 @@ class PersonalityService:
         response = self.sess.post(IBM_PI_ROOT, data=text)
         return response.json()
 
+    @staticmethod
+    def get_compatibility_score(insights_1: dict, insights_2: dict) -> float:
+        """Lower score => more compatible."""
+        sse, num_insights = PersonalityService._get_compatibility_recurse(insights_1, insights_2)
+        return math.sqrt(sse / num_insights)
+
+    @staticmethod
+    def _get_compatibility_recurse(insights_1: dict, insights_2: dict) -> Tuple[float, int]:
+        if type(insights_2) is dict:
+            if "percentile" in insights_1:
+                return (insights_1["percentile"] - insights_2["percentile"]) ** 2, 1
+            results = [PersonalityService._get_compatibility_recurse(insights_1[key], insights_2[key]) for key in insights_1.keys()]
+        elif type(insights_1) is list:
+            results = [PersonalityService._get_compatibility_recurse(insights_1[i], insights_2[i]) for i in range(len(insights_1))]
+        else:
+            return 0, 0
+        sse = sum(result[0] for result in results)
+        num_insights = sum(result[1] for result in results)
+        return sse, num_insights
+
 
 if __name__ == "__main__":
-    text = "I am sad. " * 1000
     service = PersonalityService()
-    pprint(service.get_insights(text))
+    results_sad = service.get_insights("I am sad. " * 1000)
+    results_happy = service.get_insights("I am happy. " * 1000)
+    pprint(results_sad)
+    print("Compatibility scores (lower => more compatible):")
+    print("  sad + sad   :", PersonalityService.get_compatibility_score(results_sad, results_sad))
+    print("  sad + happy :", PersonalityService.get_compatibility_score(results_sad, results_happy))
