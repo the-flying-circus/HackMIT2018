bfdf7dbed562d8878b1e0adf32c10981080f9970
Eric Wang <ezwang1@gmail.com>
2018-09-15 15:54:59 -0400

    add chat page

--- main.py
+++ main.py
@@ -30,5 +30,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+
+
 if __name__ == '__main__':
     socketio.run(app, debug=True)
