c55d9933766b164e50d9331d5cb980d0b4597b7b
Eric Wang <ezwang1@gmail.com>
2018-09-15 17:02:20 -0400

    simple cache for gif calls

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import eventlet
 import requests
+import functools
 
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, send, emit
@@ -51,17 +52,22 @@ def accounts():
     return render_template("account.html")
 
 
-@app.route("/gifs")
-def gifs():
-    query = request.args.get("q")
+@functools.lru_cache(maxsize=16)
+def get_gifs(query):
     if not query:
         resp = requests.get("https://api.giphy.com/v1/gifs/trending", params={"api_key": GIPHY_KEY})
     else:
         resp = requests.get("https://api.giphy.com/v1/gifs/search", params={"api_key": GIPHY_KEY, "q": query})
     resp.raise_for_status()
-    return jsonify({
+    return {
         "data": [x["images"]["preview_gif"]["url"] for x in resp.json()["data"]]
-    })
+    }
+
+
+@app.route("/gifs")
+def gifs():
+    query = request.args.get("q")
+    return jsonify(get_gifs(query))
 
 
 if __name__ == '__main__':
