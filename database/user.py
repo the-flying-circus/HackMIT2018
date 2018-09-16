dbfc06b16c881ecf7e1e71a071c19864b045643f
Grey Golla <bobobalink@gmail.com>
2018-09-15 23:11:44 -0400

    wrote most of the chat implementation

--- database/user.py
+++ database/user.py
@@ -11,14 +11,33 @@ class User(UserMixin, db.Model):
     access_token = db.Column(db.String(180), nullable=True, unique=True)
     is_mentor = db.Column(db.Boolean(), nullable=True)
 
+    def __repr__(self):
+        return 'User(id={}, social_id={}, nickname={}, email={}, is_mentor={}, access_token={})'.format(self.id, self.social_id, self.nickname, self.email, self.is_mentor, self.access_token)
 
-class Message(UserMixin, db.Model):
+
+class Message(db.Model):
     __tablename__ = 'messages'
     id = db.Column(db.Integer(), primary_key=True)
     sent = db.Column(db.DateTime(), nullable=False)
-    owner = db.Column(db.ForeignKey("users.id"), nullable=False)
-    recipient = db.Column(db.ForeignKey("users.id"), nullable=False)
+    owner = db.Column(db.ForeignKey("users.social_id"), nullable=False)
+    recipient = db.Column(db.ForeignKey("users.social_id"), nullable=False)
     contents = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return 'Message(id={}, sent={}, owner={}, recipient={}, contents={}'.format(self.id, self.sent, self.owner, self.recipient, self.contents)
+
+    def toDict(self):
+        return {'timestamp': self.sent, 'sender': self.owner, 'recipient': self.recipient, 'message': self.contents}
+
+
+class Conversation(db.Model):
+    __tablename__ = 'conversations'
+    id = db.Column(db.Integer(), primary_key=True)
+    mentor = db.Column(db.ForeignKey("users.social_id"), nullable=False)
+    mentee = db.Column(db.ForeignKey("users.social_id"), nullable=False)
+
+    def __repr__(self):
+        return 'Conversation(id={}, mentor={}, mentee={}'.format(self.id, self.mentor, self.mentee)
 
 
 @lm.user_loader
