b3d337c6d27b6d10cde9581858f1479e35025f2e
Eric Wang <ezwang1@gmail.com>
2018-09-15 21:22:41 -0400

    add message model

--- database/user.py
+++ database/user.py
@@ -11,6 +11,15 @@ class User(UserMixin, db.Model):
     access_token = db.Column(db.String(180), nullable=True, unique=True)
 
 
+class Message(UserMixin, db.Model):
+    __tablename__ = 'messages'
+    id = db.Column(db.Integer(), primary_key=True)
+    sent = db.Column(db.DateTime(), nullable=False)
+    owner = db.Column(db.ForeignKey("user.id"), nullable=False)
+    recepient = db.Column(db.ForeignKey("user.id"), nullable=False)
+    contents = db.Column(db.Text(), nullable=False)
+
+
 @lm.user_loader
 def load_user(key):
     return User.query.get(int(key))
