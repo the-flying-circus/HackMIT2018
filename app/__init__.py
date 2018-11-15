e97dea768a62d035906334f7289a4c1aa6678d1f
Grey Golla <bobobalink@gmail.com>
2018-11-15 13:31:46 -0800

    moved flask secret to secrets.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
+from services import secrets
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SECRET_KEY'] = 'VERY sekr1t'
